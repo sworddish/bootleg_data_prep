55810e64bb77de7c0326bb4d19bf541930a06f67
Cheng Chang <cchang@yinxiang.com>
2022-04-22 06:42:35 +0000

    fix typo and reduce parallel process

--- scripts/step4f-get-types-hierarchy.bash
+++ scripts/step4f-get-types-hierarchy.bash
@@ -7,4 +7,4 @@ source ./envs.bash
 python3 $BOOTLEG_PREP_CODE_DIR/bootleg_data_prep/wikidata/get_types_hierarchy.py \
     --data $BOOTLEG_PREP_WIKIDATA_DIR \
     --out_dir wikidata_output \
-    --processes $BOOTLEG_PREP_PROCESS_COUNT_MAXs
+    --processes $BOOTLEG_PREP_PROCESS_COUNT_MAX
